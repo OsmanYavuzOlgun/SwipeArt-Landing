f3e5c7cdffede0b07054b912c972197f6c67aea9
Osman Yavuz Olgun <osmanyavuzolgun@gmail.com>
2023-05-09 01:51:21 +0300

    SwipeArt Landing :: end of desktop view

--- main.js
+++ main.js
@@ -87,6 +87,22 @@ if (selectedTheme) {
   );
 }
 
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded");
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute("aria-expanded", "false");
+  }
+
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
+  }
+}
+
+items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
 themeButton.addEventListener("click", () => {
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
